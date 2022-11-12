59bcce7cb29ba0fb0ab285a20f34dffbd446a7f0
ParkYunHo <qkrdbsgh0921@gmail.com>
2022-11-12 23:02:19 +0900

    init

---
+++ src/main/kotlin/com/john/chatmgmt/ChatmgmtApplication.kt
@@ -0,0 +1,11 @@
+package com.john.chatmgmt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChatmgmtApplication
+
+fun main(args: Array<String>) {
+    runApplication<ChatmgmtApplication>(*args)
+}
